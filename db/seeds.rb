092410a78e30b3e2a3de743090d99b3dc621b944
andy-aguilar <aaguil3@gmail.com>
2020-06-30 09:29:09 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+20.times do
+    Ingredient.create(name: Faker::Food.unique.ingredient, quantity: Faker::Food.measurement)
+end
+
+5.times do
+    r = Recipe.create(title: Faker::Food.unique.dish )
+    r.ingredients << Ingredient.all.sample(4)
+end
